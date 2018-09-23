3181f7ef668af7723db336be7e401a94dca201ca
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-09-23 23:03:24 +0300

    Reformat.

--- src/com/tsekhanovich/challenge/containsduplicate/ContainsDuplicate.java
+++ src/com/tsekhanovich/challenge/containsduplicate/ContainsDuplicate.java
@@ -12,6 +12,7 @@ import java.util.Set;
  */
 
 public class ContainsDuplicate {
+
     public static void main(String[] args) {
         System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
         System.out.println(containsDuplicate(new int[]{1, 2, 3, 4}));
